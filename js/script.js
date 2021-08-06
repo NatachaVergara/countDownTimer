be559e54590ac589864d8c78eee331566e3bec4d
NatachaVergara <ntchvergara@gmail.com>
2021-08-06 19:33:32 -0300

    arregle bugs en js, tema validacion en campos vacios

--- js/script.js
+++ js/script.js
@@ -1,29 +1,27 @@
 let segundo = 1000,
     minuto = segundo * 60,
-    hora = minuto  * 60,
+    hora = minuto * 60,
     dia = hora * 24;
 
 let btnComenzar = document.querySelector("#comenzar");
 let h3 = document.querySelector("h3");
 
-btnComenzar.addEventListener("click", ()=>{
-    h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en:` ;
-    generarEvento();
-    
-})
+let fechaInput = document.querySelector("#fecha").value;
 
 
 
-function generarEvento(){
-    let fechaEvento = document.querySelector("#fecha").value; 
-    let evento = new Date(fechaEvento).getTime();
 
-    setInterval(()=>{
+//Generando el evento en base a la fecha actual.
+function generarEvento() {
+    let fechaEvento = document.querySelector("#fecha").value;
+    let evento = new Date(fechaEvento).getTime();//transformo la fecha ingresada a local time
+
+    setInterval(() => {
         let fechaActual = new Date().getTime();
         let cuentaRegresiva = evento - fechaActual;
 
         document.querySelector("#dia").innerText = Math.floor(cuentaRegresiva / dia);
-        
+
         document.querySelector("#hora").innerText = Math.floor((cuentaRegresiva % dia) / hora);
 
         document.querySelector("#minuto").innerText = Math.floor((cuentaRegresiva % hora) / minuto);
@@ -31,23 +29,32 @@ function generarEvento(){
         document.querySelector("#segundo").innerText = Math.floor((cuentaRegresiva % minuto) / segundo);
 
 
-        if(cuentaRegresiva < 0){
+        if (cuentaRegresiva < 0) {
             h3.innerHTML = "!Ha llegado el día!";
             let contador = document.querySelector(".contador");
-            
             contador.style.display = "none"
-        }
+        };
 
     });
 
-    
-
-
-
-    
 };
 
 
+function initApp() {
+    btnComenzar.addEventListener("click", () => {
+        let eventoInput = document.querySelector("#evento");
+        if (eventoInput.value.length == 0) {
+            alert("No deje campos vacíos");
+            setTimeout(() => {
+                location.reload()
+            }, 1)
+        }
+
+        h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en:`;
+        generarEvento(); 
+
+    });
+}
 
 
-
+initApp();
