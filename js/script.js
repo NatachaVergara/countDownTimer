5840ab3abf5a6683e8e5da9c7d30a4136101e67d
NatachaVergara <ntchvergara@gmail.com>
2021-08-12 17:26:58 -0300

    probando

--- js/script.js
+++ js/script.js
@@ -29,51 +29,53 @@ function generarEvento() {
         if (cuentaRegresiva < 0) {
             h3.innerHTML = "¡Ha llegado el día!";
             let contador = document.querySelector(".contador");
-            contador.style.display = "none"
+            contador.style.display = "none";
 
         }
+        // let obj = {
+        //     "name": document.querySelector("#evento").value,
+        //     "date": fechaEvento,
+        //     "nombreEvento": h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en:`,
+            
+        // };
 
-        
+        // localStorage.setItem("stored", JSON.stringify(obj));
 
-        let obj = {
-            "name": document.querySelector("#evento").value,
-            "date": fechaEvento,
-            "nombreEvento" : h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en:`,
 
-        };
 
-        localStorage.setItem("stored", JSON.stringify(obj));
-        
+
+
     })
 
 };
 
-function initApp() {   
-    localStorage.clear();
+function initApp() {
+
     //genero el click inicial
     btnComenzar.addEventListener("click", () => {
         h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en:`;
         generarEvento();
 
-    });       
+    });
 
-    let obj = JSON.parse(localStorage.getItem("stored"));
-    if (obj) {
-        document.querySelector("#evento").value = obj.name;
-        document.querySelector("#fecha").value = obj.date;
-        h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en: `;
-        generarEvento();
-    }
+    // let obj = JSON.parse(localStorage.getItem("stored"));
+    // if (obj) {
+    //     document.querySelector("#evento").value = obj.name;
+    //     document.querySelector("#fecha").value = obj.date;
+    //     h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en: `;
 
-    
+    //     generarEvento();
+    // }
 
-    //Boton para borrar el evento y que se refresque la pantalla
-    btnBorrar.addEventListener("click", () => {
-        localStorage.clear();
-        location.reload();
 
-    })
-    
+
+    // //Boton para borrar el evento y que se refresque la pantalla
+    // btnBorrar.addEventListener("click", () => {
+    //     localStorage.clear();
+    //     location.reload();
+
+    // })
+
 
 }
 
