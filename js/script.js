11709a94e2a2cf933fcd176f74ce4f07f06527af
NatachaVergara <ntchvergara@gmail.com>
2021-08-12 11:58:38 -0300

    probando compartir el evento

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let segundo = 1000,
     dia = hora * 24;
 
 let btnComenzar = document.querySelector("#comenzar");
+let btnBorrar = document.querySelector("#borrar");
 let h3 = document.querySelector("h3");
 
 //Generando el evento en base a la fecha actual.
@@ -29,21 +30,55 @@ function generarEvento() {
             h3.innerHTML = "¡Ha llegado el día!";
             let contador = document.querySelector(".contador");
             contador.style.display = "none"
+
+        }
+
+        let obj = {
+            "name": document.querySelector("#evento").value,
+            "date": fechaEvento,
+            "nombreEvento" : h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en:`,
+
         };
 
-    });
+        localStorage.setItem("stored", JSON.stringify(obj));
+    })
 
 };
+
 function initApp() {
+    
     //genero el click inicial
     btnComenzar.addEventListener("click", () => {
         h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en:`;
         generarEvento();
 
-    });
+    });   
+    
+
+    let obj = JSON.parse(localStorage.getItem("stored"));
+    if (obj) {
+        document.querySelector("#evento").value = obj.name;
+        document.querySelector("#fecha").value = obj.date;
+        h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en: `;
+        generarEvento();
+    }
+
+    
+
+    //Boton para borrar el evento y que se refresque la pantalla
+    btnBorrar.addEventListener("click", () => {
+        localStorage.removeItem("stored");
+        location.reload();
+
+    })
+    
+
 }
+
+
+
+
 initApp()
 
 
 
- 
