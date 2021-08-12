806d4c9bb730496d39a9f230e42be22881a610ce
NatachaVergara <ntchvergara@gmail.com>
2021-08-12 12:02:02 -0300

    probando compartir el evento

--- js/script.js
+++ js/script.js
@@ -33,14 +33,14 @@ function generarEvento() {
 
         }
 
-        let obj = {
-            "name": document.querySelector("#evento").value,
-            "date": fechaEvento,
-            "nombreEvento" : h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en:`,
+        // let obj = {
+        //     "name": document.querySelector("#evento").value,
+        //     "date": fechaEvento,
+        //     "nombreEvento" : h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en:`,
 
-        };
+        // };
 
-        localStorage.setItem("stored", JSON.stringify(obj));
+        // localStorage.setItem("stored", JSON.stringify(obj));
     })
 
 };
@@ -55,13 +55,13 @@ function initApp() {
     });   
     
 
-    let obj = JSON.parse(localStorage.getItem("stored"));
-    if (obj) {
-        document.querySelector("#evento").value = obj.name;
-        document.querySelector("#fecha").value = obj.date;
-        h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en: `;
-        generarEvento();
-    }
+    // let obj = JSON.parse(localStorage.getItem("stored"));
+    // if (obj) {
+    //     document.querySelector("#evento").value = obj.name;
+    //     document.querySelector("#fecha").value = obj.date;
+    //     h3.innerHTML = ` Su ${document.querySelector("#evento").value} comenzara en: `;
+    //     generarEvento();
+    // }
 
     
 
