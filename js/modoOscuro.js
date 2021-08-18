60f053295ff682ad214bdae0b7e999b22a31d1a3
NatachaVergara <ntchvergara@gmail.com>
2021-08-18 11:53:43 -0300

    Agregando el modo noche/dia

--- js/modoOscuro.js
+++ js/modoOscuro.js
@@ -5,6 +5,7 @@ let btnModo = document.querySelector("#btnModo")
 btnModo.addEventListener("click", () =>{
 
         body.classList.toggle('modoOscuro');
+        modalModoOscuro.classList.toggle('modoOscuroModal') ;      
         
         btnModo.classList.toggle(`btnModoOscuro`);
         if(btnModo.innerHTML === "Modo Noche 🌛"){
